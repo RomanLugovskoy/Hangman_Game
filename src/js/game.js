5d86c6b26590d5f0608154abefd73b10e38b295b
RomanLugovskoy <romagma29@gmail.com>
2024-09-17 08:19:41 +0300

    gameplay

--- src/js/game.js
+++ src/js/game.js
@@ -2,6 +2,19 @@ import { WORDS } from "./consts";
 import { KEYBOARD_LETTERS } from "./consts";
 
 const gameDiv = document.getElementById("game");
+const logoElem = document.getElementById("logo");
+let triesLeft;
+let winCounter;
+
+const createHangmanImg = () => {
+  const image = document.createElement("img");
+  image.alt = "hangman image";
+  image.id = "hangmanImg";
+  image.src = "/images/hg-0.png";
+  image.classList.add("hangman-img");
+  return image;
+};
+
 const createPlaceholders = () => {
   const wordToGuess = sessionStorage.getItem("word");
   const wordArray = Array.from(wordToGuess);
@@ -28,14 +41,45 @@ const createKeyboard = () => {
   return keyboard;
 };
 
+const checkLetter = (letter) => {
+  const word = sessionStorage.getItem("word");
+  const inputLetter = letter.toLowerCase();
+  if (word.includes(inputLetter)) {
+    const wordArr = Array.from(word);
+    wordArr.forEach((currentLetter, i) => {
+      if (currentLetter === inputLetter) {
+        document.getElementById(`letter_${i}`).innerText =
+          inputLetter.toUpperCase();
+      }
+    });
+  } else {
+    const triesCounter = document.getElementById("tries-counter");
+    triesLeft -= 1;
+    triesCounter.innerText = triesLeft;
+    const image = document.getElementById("hangmanImg");
+    image.src = `/images/hg-${10 - triesLeft}.png`;
+  }
+};
+
 export const startGame = () => {
+  triesLeft = 10;
+  logoElem.classList.add("logo-sm");
   const randomIndex = Math.floor(Math.random() * WORDS.length);
   const word = WORDS[randomIndex];
   sessionStorage.setItem("word", word);
+
   gameDiv.innerHTML = createPlaceholders();
+
+  gameDiv.innerHTML += `<p class="tries-left font-medium mt-5">TRIES LEFT: <span class="text-red-600 font-medium" id="tries-counter">10</span></p>`;
+
   const keyboardDiv = createKeyboard();
   keyboardDiv.addEventListener("click", (e) => {
-    console.log(e.target.id);
+    if (e.target.tagName.toLowerCase() === "button") {
+      e.target.disabled = true;
+      checkLetter(e.target.id);
+    }
   });
   gameDiv.appendChild(keyboardDiv);
+  const hangmanImg = createHangmanImg();
+  gameDiv.prepend(hangmanImg);
 };
